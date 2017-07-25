408301de5914a61c4e0eba6ba78944409d270da9
Resi Respati <resir014@gmail.com>
2017-07-25 23:08:45 +0700

    Get tests to work initially.

--- test/components/home.spec.js
+++ test/components/home.spec.js
@@ -1,20 +1,20 @@
-import Component from '../src/Component.jsx';
-import TestUtils from 'react-addons-test-utils';
+/* eslint-env node, mocha */
+
+import Component from '../../src/js/components/home.jsx';
+import TestUtils from 'react-dom/test-utils';
 import React from 'react';
 
-var component;
-var spy = sinon.spy();
-
 describe('Given an instance of the Component', () => {
+  var component;
+
   describe('when we render the component', () => {
-    before(() => {
-      component = TestUtils.renderIntoDocument(<Component onRender={ spy } />);
+    beforeEach(() => {
+      component = TestUtils.renderIntoDocument(<Component />);
     });
     it('should render a paragraph', () => {
       var paragraph = TestUtils.scryRenderedDOMComponentsWithTag(component, 'p');
 
       expect(paragraph).to.have.length.above(0, 'Expected to have element with tag <p>');
-      expect(spy).to.be.calledOnce;
     });
   });
 });
